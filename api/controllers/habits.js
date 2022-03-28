3b5aba323d183ec1cffc20583d9d2550c877c246
dimi-fn <dimitris_fanis@protonmail.com>
2022-03-28 17:24:47 +0300

    set up habits routes

---
+++ api/controllers/habits.js
@@ -0,0 +1,45 @@
+const Habit = require('../models/Habit');
+
+// index route: gets all habits
+async function index (req, res) {
+    try {
+        const habits = await Habit.all
+        res.status(200).json(habits)
+    } catch(err) {
+        res.status(500).json(err)
+    }
+};
+
+// show route: gets habit by id
+async function show (req, res) {
+    try {
+        const habit = await Habit.findById(req.params.id);
+        res.status(200).json(habit)
+    } catch(err) {
+        res.status(404).json({err})
+    }
+};
+
+/* router.post('/', habitRoutes.create); // creates habit route */
+async function create (req, res) {
+    try {
+        const habit = await Habit.create(req.body);
+        res.status(201).json(habit)
+    } catch (err) {a
+        res.status(422).json({err})
+    }
+};
+
+// router.delete('/:id', habitRoutes.destroy); //endpoint for the delete habit
+
+async function destroy (req, res) {
+    try {
+    const habit = await Habit.destroy(req.params.id);
+    const resp = await habit.destroy();
+    res.status(204).send(resp)
+} catch (err) {
+    res.status(404).json({err})
+}
+}
+
+module.exports= {index, show, create, destroy};
