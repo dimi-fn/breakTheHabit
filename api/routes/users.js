6037f0bc2d4f2c6756639447d41909a959a1d149
dimi-fn <dimitris_fanis@protonmail.com>
2022-03-29 14:10:47 +0300

    added route to retrieve all habits per unique user

--- api/routes/users.js
+++ api/routes/users.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const userRoutes = require('../controllers/users');
 
 router.get('/', userRoutes.index); // gets all users
-router.get('/:id', userRoutes.show); // gets user by id
+router.get('/:id', userRoutes.show); // gets user by user id
+router.get('/:id/habits', userRoutes.showHabitsPerUser); // gets all habits per user id
 router.post('/', userRoutes.create); // creates user post route
 
 
