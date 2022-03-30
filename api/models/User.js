70e7fdefca8b06d9200caa9b873e1d734cdf15ea
dimi-fn <dimitris_fanis@protonmail.com>
2022-03-30 23:07:37 +0300

    refactored in backend

--- api/models/User.js
+++ api/models/User.js
@@ -82,7 +82,7 @@ class User{
         return new Promise(async (res, rej) => {
             try {
                 let result = await db.run(SQL`SELECT * FROM users
-                                                WHERE email = ${email};`);
+                                                WHERE email = $1;`, [email]);
                 let user = new User(result.rows[0])
                 res(user)
             } catch (err) {
