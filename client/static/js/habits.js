4b3675b8efc75d90f35344524484d2ef377af1e7
Abdulrahman Awad <abdul.hmha@gmail.com>
2022-03-31 15:16:50 +0100

    habits.js and habits.html modification

--- client/static/js/habits.js
+++ client/static/js/habits.js
@@ -1,32 +1,104 @@
 const habitButton = document.getElementById("addHabitBtn")
 
-var list1 = [], list2 = [], list3 = [];
+var list1 = [], list2 = [], list3 = [], list4 = [], list5 = [];
 
 var n = 1, x = 0;
 
 // async function addRowAndSubmitHabits
 
+function calcProg(goal_freq,cum_freq){
+    let n = Math.round(((goal_freq-cum_freq)/goal_freq))
+    return n;
+}
+
 function AddRow(){
+
+    let progPercVal = 0, progVal = 0;    
+
     var AddRown = document.getElementById('habitTable');
     var NewRow = AddRown.insertRow(n);
 
-list1[x] = document.getElementById("habit").value;
-list2[x] = document.getElementById("frequency").value;
-list3[x] = document.getElementById("units").value;
-list4[x] = document.getElementById("time").value;
+    list1[x] = document.getElementById("habit").value;
+    list2[x] = document.getElementById("frequency").value;
+    list3[x] = document.getElementById("units").value;
+    list4[x] = document.getElementById("time").value;
+    list5[x];
 
-var cel1 = NewRow.insertCell(0);
-var cel2 = NewRow.insertCell(1);
-var cel3 = NewRow.insertCell(2);
-var cel4 = NewRow.insertCell(3);
+    var cel1 = NewRow.insertCell(0);
+    var cel2 = NewRow.insertCell(1);
+    var cel3 = NewRow.insertCell(2);
+    var cel4 = NewRow.insertCell(3);
+    var cel5 = NewRow.insertCell(4);
+    var cel6 = NewRow.insertCell(5);
+    var cel7 = NewRow.insertCell(6);
 
-cel1.innerHTML = list1[x];
-cel2.innerHTML = list2[x];
-cel3.innerHTML = list3[x];
-cel4.innerHTML = list4[x];
+    cel1.innerHTML = list1[x];
+    cel2.innerHTML = list2[x];
+    cel3.innerHTML = list3[x];
+    cel4.innerHTML = list4[x];
+    cel5.innerHTML = `<div class = "progress">
+                      <div class="progress-fill">
+                      </div>
+                      </div>
+                      <span id=progText>${progVal}</span><br>
+                      <span id="progPercText">${progPercVal}%</span>`
+    cel6.innerHTML = `<td><button data-bs-toggle="modal" data-bs-target="#editHabitModal" id = "editBtn">Edit</button></td>
+                      `;
+    cel7.innerHTML = `<tr><button class = "deleteBtn">Delete</button></tr>`
 
-n++;
-x++;
+    n++; x++;
+    editHabit()
+    const tableEl = document.querySelector("#habitTable")
+    function onDeleteRow (e) {
+        if (!e.target.classList.contains('deleteBtn')){
+            return;
+        }
+    
+        const btn = e.target;
+        btn.closest("tr").remove();
+    }
+    tableEl.addEventListener("click", onDeleteRow);
+
+
+} //AddRow function ends
+
+function editHabit(){
+    let editBtn = document.getElementById('editBtn')
+    editBtn.addEventListener('click', () => {
+        let t = document.getElementById('editHabitText').value
+        let g = 
+        calcProg(g,t)
+    })
 }
 
 habitButton.addEventListener("click",AddRow);
+
+const logoutBtn = document.getElementById('logoutbtn')
+logoutBtn.addEventListener('click', () => {
+    window.location.href = "index.html"
+    localStorage.clear();
+})
+
+// const formEl = document.querySelector('form');
+// const tbodyEl = document.getElementById('tbody');
+// function onAddWebsite(e){
+//     e.preventDefault();
+//     const habbit = document.getElementById("habit")
+//     const freq = document.getElementById("frequency").value;
+//     const unit = document.getElementById("units").value;
+//     const time = document.getElementById("time").value;
+//     tbodyEl.innerHTML +=`
+//      <tr>
+//          <td>${Add habit}</td>
+//          <td>${Choose your goal frequency with a number}</td>
+//          <td>${Choose Units}</td>
+//          <td>${Choose period of time}</td>
+//          <td><button class = "DEL">Delete</button></td>
+
+//      </tr>
+//      `;
+
+
+// }
+
+// habitButton.addEventListener("click", onAddWebsite);
