2b8b736d2e4f296fab6207c4bfbf07d6b6cdc991
dimi-fn <dimitris_fanis@protonmail.com>
2022-03-31 20:51:43 +0300

    procfile and db config for deployment

--- api/dbConfig/init.js
+++ api/dbConfig/init.js
@@ -1,5 +1,22 @@
+// const { Pool } = require("pg");
+
+// const pool = new Pool();
+
+// module.exports = pool;
+
 const { Pool } = require("pg");
 
-const pool = new Pool();
+let config;
+
+if(process.env.DATABASE_URL){
+    config = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+}
+
+const pool = new Pool(config);
 
 module.exports = pool;
